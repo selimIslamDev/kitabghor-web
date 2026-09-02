fb554b732e88014589e061bd357c1018810d1ce7
Salim <salimislam0036@gmail.com>
2026-09-02 23:12:53 +0600

    refactor(ComboSection): remove commented-out cards skeleton code

--- components/home/ComboSection.tsx
+++ components/home/ComboSection.tsx
@@ -84,7 +84,6 @@ export default function ComboSection() {
             <div className="h-4 bg-[#1a1815] rounded w-72 mx-auto animate-pulse" />
           </div>
 
-          {/* Cards skeleton */}
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-10">
             {[...Array(3)].map((_, i) => (
               <div
